c2a11ae74bb17ef21889fbc28cc58e040dbe30d3
kathyskead <kathyskead@gmail.com>
2015-08-20 20:40:47 -0400

    edits

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# getting-cleaning-data-project
+## Getting and Cleaning Data Course Project
+This script prepares a tidy data set from data collected from accelerometers from the Samsung Galaxy S smartphone. Find a full description of the data here: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+#Step 1:
+The script cleans up your work space.
+Loads the "dplyr" library and sets the working directory.
+
+#Step 2:
+The following datasets are read into R with the read.table() function:
+test/subject_test.txt
+train/subject_train.txt
+
+test/X_test.txt
+train/X_train.txt
+
+test/y_test.txt
+train/y_train.txt
+
+features.txt
+activity_labels.txt
+
+#Step 3:
+The row_bind() function is used to combine datasets:
+The subject datasets are combined into one dataset named "subjectData"
+The x table datasets are combined into one dataset named "xData".
+The y table datasets are combined into one dataset named "yData".
+
+#Step 4:
+The names() function is used to give variables in the datasets names:
+The variable in "subjectData" is named "subject".
+The variable in "yData" is named "activity"
+The variables in "xData" are assigned names from the features.txt dataset.
+
+#Step 5:
+The subjectData, xData and yData are then combined into a single dataset "data" by useing the bind.cols() function.
+
+#Step 6:
+All duplicate variable are dropped from the "data" dataset using the !duplicated() function.
+
+#Step 7:
+Only the measurements on the mean and standard deviation for each measurement are extract from the dataset by selecting the following variables: activity, subject and any variables containing the names "mean" and "std".
+
+#Step 8:
+Descriptive activity names from activity_labels.txt are used to assign names to the activity variable by using the factor() function.
+
+#Step 9:
+The data is appropriately labeled using descriptive variable names with the gsub() function.
+
+#Step 10:
+A second, independent tidy data set with the average of each variable for 
+each activity and each subject is created using the aggregate() function.
+
+#Step 11:
+Finally a tidydata.txt file is written using the write.table() function.
--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,11 @@
-#clean up work space
+# Clean up work space
 rm(list=ls())
 
-#load necessary libraries
+# Load necessary libraries
 library(dplyr)
 setwd("C:\\Users\\mskead\\Google Drive\\Data Science Course\\Getting and cleaning data\\Data\\UCI HAR Dataset\\data\\UCI HAR Dataset")
 
-#read tables into R
+# Read tables into R
 subjectTest <- read.table("test/subject_test.txt")
 subjectTrain <- read.table("train/subject_train.txt")
 xTest <- read.table("test/X_test.txt")
@@ -15,31 +15,30 @@ yTrain <- read.table("train/y_train.txt")
 featureNames <- read.table("features.txt")
 activity_lables <- read.table("activity_labels.txt")
 
-#combine tables
+# Combine tables
 subjectData <- bind_rows(subjectTest, subjectTrain)
 xData <- bind_rows(xTest, xTrain)
 yData <- bind_rows(yTest, yTrain)
 
-#name variables
+# Name variables
 names(subjectData) <- c("subject")
 names(yData) <- c("activity")
 names(xData) <- featureNames$V2
 
-#combine columns to get one data set
+# Combine columns to get one data set
 x <- bind_cols(subjectData,yData)
 data<- bind_cols(xData, x)
 
-#drop duplicate columns
+# Drop duplicate columns
 data <- data[ !duplicated(names(data)) ]
 
-#select only the following variables: activity, subject and those containing "mean" and "std"
+# Select only the following variables: activity, subject and those containing "mean" and "std"
 data <- select(data, activity, subject, contains("mean"), contains("std"))
 
-
-#Add descriptive activity names from activity_labels.txt to name activities
+# Add descriptive activity names from activity_labels.txt to name activities
 data$activity <- factor(data$activity, levels = activity_lables[,1], labels = activity_lables[,2])
 
-#Across all columns replace an abbreviation with full descriptive word
+# Across all columns replace an abbreviation with full descriptive word
 names(data) <- gsub("^t","Time", names(data))
 names(data) <- gsub("Acc","Accelerometer", names(data))
 names(data) <- gsub("Gyro","Gyroscope", names(data))
@@ -48,7 +47,9 @@ names(data) <- gsub("^f","Frequency", names(data))
 names(data) <- gsub("BodyBody","Body", names(data))
 
 
-#create a second, independent tidy data set with the average of each variable for 
+# Create a second, independent tidy data set with the average of each variable for 
 #each activity and each subject.
 tidydata <- aggregate(.~activity + subject, data, mean)
+
+# Create a tidydata.txt file
 write.table(tidydata, file = "tidydata.txt",row.name=FALSE)
